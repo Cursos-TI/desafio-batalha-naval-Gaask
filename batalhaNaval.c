5573085f1d5672e84d21ec293e5d3cf67248037d
Gabriel Nalesso <121267637+Gaask@users.noreply.github.com>
2025-02-21 16:28:16 +0000

    adição dos navios, 3 posições

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,94 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int linha, linhaJ, coluna, colunaJ, valido = 0; // linhaJ é o que é exibido, linha é o que de fato é calculado
+int tabuleiro[10][10] = {0};                    // Inicializa um tabuleiro 10x10. = 0 indica que todos os valores do tabuleiro são 0.
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+// Função para imprimir o tabuleiro
+void imprimirTabuleiro(int tabuleiro[10][10]) // Função que chama a matriz como argumento
+{
+  for (int i = 0; i < 10; i++) // Loop para percorrer e definir as 10 linhas como 0
+  {
+    for (int j = 0; j < 10; j++) // Loop para percorrer e definir as 10 colunas como 0
+    {
+      printf("%d ", tabuleiro[i][j]); // Imprime o tabuleiro com um espaço entre os "0" e "3"
+      // Se o \n estiver aqui, a saída será "0" linha por linha
+    }
+    printf("\n"); // Pula a linha dentro do loop de linha, para formatar o tabuleiro como um quadrado
+  }
+}
+void navio1()
+{
+  do
+  {
+    printf("\n\nOnde você quer posicionar seu navio? Digite a linha de 1 a 10: ");
+    scanf("%d", &linha);
+    printf("\nAgora, digite a coluna de 1 a 10: ");
+    scanf("%d", &coluna);
+    if (linha <= 10 && linha > 0 && coluna <= 10 && coluna > 0)
+    {
+      linhaJ = linha;
+      colunaJ = coluna;
+      tabuleiro[linha - 1][coluna - 1] = 3;
+      printf("\nVocê selecionou: linha %d, coluna %d.\n\n", linhaJ, colunaJ);
+      valido = 1;
+      if (coluna > 8)
+      {
+        tabuleiro[--linha][coluna - 2] = 3; // em matrizes, a linha 1 do jogador é = linha 0, por isso, decremento
+        tabuleiro[linha][coluna - 3] = 3;   // as colunas são decrementadas para posicionar o navio horizontamente
+      }
+      else
+      {
+        tabuleiro[--linha][coluna] = 3;
+        tabuleiro[linha][++coluna] = 3;
+      }
+    }
+    else
+    {
+      printf("posições inválidas.\n");
+    }
+  } while (valido != 1);
+  imprimirTabuleiro(tabuleiro);
+}
+void navio2()
+{
+  do
+  {
+    printf("\n\nOnde você quer posicionar seu segundo navio? Digite a linha de 1 a 10: ");
+    scanf("%d", &linha);
+    printf("\nAgora, digite a coluna de 1 a 10: ");
+    scanf("%d", &coluna);
+    if (coluna > 0 && coluna <= 10 && linha > 0 && linha <= 10)
+    {
+      linhaJ = linha;
+      colunaJ = coluna;
+      tabuleiro[linha - 1][coluna - 1] = 3;
+      printf("\nVocê selecionou: linha %d, coluna %d.\n\n", linhaJ, colunaJ);
+      valido = 1;
+      if (linha < 8)
+      {
+        tabuleiro[linha][--coluna] = 3;
+        tabuleiro[++linha][coluna] = 3;
+      }
+      else
+      {
+        tabuleiro[linha - 2][--coluna] = 3;
+        tabuleiro[linha - 3][coluna] = 3;
+      }
+    }
+    else
+    {
+      printf("posições inválidas.\n");
+    }
+  } while (valido != 1);
+  imprimirTabuleiro(tabuleiro);
+}
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
+int main()
+{
+  printf("- - - VOCÊ ESTÁ JOGANDO BATALHA NAVAL - - -\n");
+  printf("O tabuleiro é 10x10. O primeiro passo é você escolher onde posicionar o seu navio.\n\n");
+  imprimirTabuleiro(tabuleiro);
+  navio1();
+  navio2();
+  return 0;
 }
